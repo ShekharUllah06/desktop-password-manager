9876abaf21fc6701758838d44b826517e6fe3508
shekhar ullah <ullah.shekhar@gmail.com>
2017-03-29 00:30:21 +0600

    initial commit

---
+++ src/passwordmanager/bean/AccountInfo.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2017 Abdullah Shekhar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package passwordmanager.bean;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Abdullah Shekhar
+ */
+@Entity
+@Table(name = "account_info")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "AccountInfo.findAll", query = "SELECT a FROM AccountInfo a"),
+    @NamedQuery(name = "AccountInfo.findById", query = "SELECT a FROM AccountInfo a WHERE a.id = :id"),
+    @NamedQuery(name = "AccountInfo.findByUserName", query = "SELECT a FROM AccountInfo a WHERE a.userName = :userName"),
+    @NamedQuery(name = "AccountInfo.findByPassword", query = "SELECT a FROM AccountInfo a WHERE a.password = :password"),
+    @NamedQuery(name = "AccountInfo.findByType", query = "SELECT a FROM AccountInfo a WHERE a.type = :type"),
+    @NamedQuery(name = "AccountInfo.findByWebsite", query = "SELECT a FROM AccountInfo a WHERE a.website = :website")})
+public class AccountInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "type")
+    private String type;
+    @Column(name = "website")
+    private String website;
+
+    public AccountInfo() {
+    }
+
+    public AccountInfo(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof AccountInfo)) {
+            return false;
+        }
+        AccountInfo other = (AccountInfo) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "passwordmanager.bean.AccountInfo[ id=" + id + " ]";
+    }
+    
+}
